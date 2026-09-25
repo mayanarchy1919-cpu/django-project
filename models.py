fa0674b8c66fae52eeb6e68b33fbf8a633b619e2
mayanarchy1919-cpu <mayanarchy1919@gmail.com>
2026-09-25 21:03:46 +0400

    Adding Training models

---
+++ models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Training(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    date = models.DateTimeField()
+
+    location = models.CharField(
+        max_length=200,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
